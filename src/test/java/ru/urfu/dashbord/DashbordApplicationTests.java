85cabe3db7dbd92558c7a02dea6d7c25532789b3
Koval Dmitriy <D.Koval@hostco.ru>
2023-11-06 15:09:43 +0500

    init commit

---
+++ src/test/java/ru/urfu/dashbord/DashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.dashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
