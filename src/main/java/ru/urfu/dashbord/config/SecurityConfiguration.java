8468ddd3bada743e4ab81cfa943f2160835170ed
Koval Dmitriy <D.Koval@hostco.ru>
2023-11-09 08:41:43 +0500

    keycloak 1.0

--- src/main/java/ru/urfu/dashbord/config/SecurityConfiguration.java
+++ src/main/java/ru/urfu/dashbord/config/SecurityConfiguration.java
@@ -1,39 +1,50 @@
-package ru.urfu.dashbord.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
-
-  @Bean
-  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http
-        .oauth2Client()
-        .and()
-        .oauth2Login()
-        .tokenEndpoint()
-        .and()
-        .userInfoEndpoint();
-
-    http
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-
-    http
-        .authorizeHttpRequests()
-        .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
-        .anyRequest()
-        .fullyAuthenticated()
-        .and()
-        .logout()
-        .logoutSuccessUrl("http://localhost:8080/realms/dashboard_realm/protocol/openid-connect/logout?redirect_uri=http://localhost:8081/");
-
-    return http.build();
-  }
-}
+//package ru.urfu.dashbord.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.oauth2.client.registration.ClientRegistration;
+//import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+//import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+//import org.springframework.security.oauth2.core.AuthorizationGrantType;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfiguration {
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//    http.cors().and().csrf().disable().authorizeRequests()
+//        .antMatchers("asdasd").permitAll()
+//        .anyRequest().authenticated()
+//        .and().oauth2Login();
+////    httpSecurity.authorizeRequests()
+////        .requestMatchers()..permitAll()
+////        .anyRequest().authenticated()
+////        .and()
+////        .oauth2Login();
+//    return http.build();
+//  }
+//
+//  @Bean
+//  public ClientRegistrationRepository clientRepository() {
+//
+//    ClientRegistration keycloak = keycloakClientRegistration();
+//    return new InMemoryClientRegistrationRepository(keycloak);
+//  }
+//
+//  private ClientRegistration keycloakClientRegistration() {
+//
+//    return ClientRegistration.withRegistrationId("dashboard_realm")
+//        .clientId("my-client")
+//        .redirectUri("http://localhost:8081")
+//        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+//        .issuerUri("http://localhost:8080/realms/dashboard_realm")
+//        .authorizationUri("http://localhost:8080/realms/dashboard_realm/protocol/openid-connect/auth")
+//        .tokenUri("http://localhost:8080/realms/dashboard_realm/protocol/openid-connect/token")
+//        .userInfoUri("http://localhost:8080/realms/dashboard_realm/protocol/openid-connect/userinfo")
+//        .build();
+//  }
+//}
--- src/main/java/ru/urfu/dashbord/controller/SampleController.java
+++ src/main/java/ru/urfu/dashbord/controller/SampleController.java
@@ -1,6 +1,7 @@
 package ru.urfu.dashbord.controller;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,21 +18,19 @@ public class SampleController {
   }
 
   @GetMapping("/user")
-  @Secured({"USER"})
-//  @PreAuthorize("hasRole('USER')")
+  @PreAuthorize("hasRole('USER')")
   public String getUserInfo() {
     return "user info";
   }
 
   @GetMapping("/admin")
-  @Secured({"ADMIN"})
-//  @PreAuthorize("hasRole('ADMIN')")
+  @PreAuthorize("hasRole('ADMIN')")
   public String getAdminInfo() {
     return "admin info";
   }
 
   @GetMapping("/service")
-//  @PreAuthorize("hasRole('SERVICE')")
+  @PreAuthorize("hasRole('SERVICE')")
   public String getServiceInfo() {
     return "service info";
   }
@@ -39,6 +38,7 @@ public class SampleController {
   @GetMapping("/me")
   public Object getMe() {
     final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
     return authentication.getName();
   }
 }
